5d29dd9b5ea5b42a1e5b33ea53c2427554723b74
inix84 <inix84@mail.ru>
2024-10-20 21:37:32 +0500

    3 задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1. Присвоить 4 строковых переменных и вывести в консоль:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+        System.out.println("Задача 2. ФИО верхним регистром:");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println();
+        System.out.println("Задача 3. ФИО верхним регистром:");
+        String fullName3 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника —  " + fullName3.replace('ё','е'));
+
     }
 }
