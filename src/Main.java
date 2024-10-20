5ab947bdf273f63748da90eda252b02978069ee0
inix84 <inix84@mail.ru>
2024-10-20 21:41:19 +0500

    3 задачи

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Задача 2. ФИО верхним регистром:");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
         System.out.println();
-        System.out.println("Задача 3. ФИО верхним регистром:");
+        System.out.println("Задача 3. ё заменить на е:");
         String fullName3 = "Иванов Семён Семёнович";
         System.out.println("Данные Ф. И. О. сотрудника —  " + fullName3.replace('ё','е'));
 
